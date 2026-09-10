6e88a171a3bd19743b7e566e7e82259591553415
german-rs <ing.germanriveros@gmail.com>
2026-09-10 08:25:34 -0300

    Configura dominio profegerman.cl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://german-rs.github.io',
-  base: '/profegerman',
+  site: "https://profegerman.cl",
 });
