a458dca698634465cb2928b8b151acaae0e9f8dd
german-rs <ing.germanriveros@gmail.com>
2026-09-10 14:40:51 -0300

    Base del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://profegerman.cl",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
