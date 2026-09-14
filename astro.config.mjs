483a6ea4b7939c6589c58239ebf93b3770b4743a
german-rs <ing.germanriveros@gmail.com>
2026-09-14 17:28:20 -0300

    feat: instalar GTM vía Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://profegerman.cl",
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push", "gtag"],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
