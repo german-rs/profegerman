f19118bd109716e25db95f0213ba584fa092a3bf
german-rs <ing.germanriveros@gmail.com>
2026-09-17 16:30:25 -0300

    Se asigna versión canónica

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://profegerman.cl",
+  trailingSlash: 'always',
   integrations: [
     partytown({
       config: {
